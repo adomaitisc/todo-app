cf2a7e0b2fbd50e250bfd023e7236b9acdbf074f
Cauã Adomaitis <caadomaitis@gmail.com>
2022-08-05 19:54:53 -0300

    basic prisma pscale and next app working :)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,30 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import { trpc } from "../utils/trpc";
 
-// https://www.youtube.com/watch?v=PKy2lYEnhgs&t=3941s
-// https://www.prisma.io/docs/getting-started/setup-prisma/add-to-existing-project/relational-databases/introspection-typescript-postgres
-// https://trpc.io/docs/nextjs
-// https://app.planetscale.com/caadomaitis/list-of-tasks
-// https://www.prisma.io/docs/concepts/components/prisma-schema
+// pscale connect list-of-tasks main
+// npm run dev
+// npx prisma studio
 
 const Home: NextPage = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
     <div id="page" className="w-screen h-screen p-24 bg-zinc-200">
+      <FormModal isOpen={isModalOpen} close={() => setModalOpen(false)} />
       <div
         id="content"
         className="w-full h-full p-12 bg-zinc-100 shadow-lg rounded-3xl flex flex-col items-start justify-start"
       >
-        <h1 className="text-zinc-800 text-3xl font-medium">Bem vindo Cauã,</h1>
+        <div className="w-full flex flex-row justify-between">
+          <h1 className="text-zinc-800 text-3xl font-medium">-</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="py-2 px-4 text-blue-700 text-sm font-medium hover:text-gray-900"
+          >
+            Nova Lista
+          </button>
+        </div>
         <div className="mt-8"></div>
         <p className="text-zinc-800 text-2xl font-medium">suas listas:</p>
         <div className="mt-4"></div>
@@ -22,13 +32,100 @@ const Home: NextPage = () => {
           {/* Where we will render the lists */}
           <div className="w-72 h-32 p-8 bg-zinc-100 shadow rounded-lg flex flex-col items-start justify-between duration-100 hover:scale-105">
             <div className="w-full flex flex-row items-center justify-between">
-              <h1 className="text-lg text-zinc-800">qi-website</h1>
+              <h1 className="text-lg text-zinc-800">-</h1>
               <p className="text-xl text-zinc-700 font-bold">100%</p>
             </div>
             <div className="w-full h-1 rounded-full bg-blue-500"></div>
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+
+type FormModalProps = {
+  isOpen: boolean;
+  close: () => void;
+};
+
+const FormModal = ({ isOpen, close }: FormModalProps) => {
+  const initialValues = { listTitle: "", listDescription: "" };
+  const [formData, setFormData] = useState(initialValues);
+
+  const createListOfTasks = trpc.useMutation(["create-list-of-tasks"]);
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+    createListOfTasks.mutate({
+      listTitle: formData.listTitle,
+      listDescription: formData.listDescription,
+      listCompletion: 0,
+    });
+    close();
+  };
+
+  const handleClose = (e: any) => {
+    setFormData(initialValues);
+    close();
+  };
+
+  return (
+    <div
+      className={`${
+        !isOpen && `hidden`
+      } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 bottom-0 z-10 w-full flex justify-center items-center bg-zinc-900/70`}
+    >
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col w-96 px-8 py-6 bg-zinc-200 rounded-lg"
+      >
+        <button
+          type="button"
+          onClick={handleClose}
+          className="self-end text-sm text-red-700 font-medium hover:text-gray-900"
+        >
+          Cancelar
+        </button>
+        <div className="flex-flex-col w-full">
+          <label className="block mb-1 text-sm font-medium text-zinc-600">
+            Título:
+          </label>
+          <input
+            className="bg-zinc-200 border border-zinc-300 text-zinc-900 placeholder-zinc-400 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            type="text"
+            name="listTitle"
+            placeholder="ex: todo-app"
+            onChange={handleChange}
+            required
+            value={formData.listTitle}
+          />
+        </div>
+        <div className="mt-4"></div>
+        <div className="flex-flex-col w-full">
+          <label className="block mb-1 text-sm font-medium text-zinc-600">
+            Descrição:
+          </label>
+          <input
+            className="bg-zinc-200 border border-zinc-300 text-zinc-900 placeholder-zinc-400 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            type="text"
+            name="listDescription"
+            placeholder="ex: site para organizar tarefas"
+            onChange={handleChange}
+            required
+            value={formData.listDescription}
+          />
+        </div>
+        <div className="mt-4"></div>
+        <button className="self-start text-sm text-blue-700 font-medium px-1 hover:text-gray-900">
+          Criar Lista
+        </button>
+      </form>
     </div>
   );
 };
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,24 +3,34 @@ import { z } from "zod";
 
 import { prisma } from "../utils/prisma";
 
-export const appRouter = trpc.router().query("create-list-of-tasks", {
-  input: z.object({
-    listTitle: z.string(),
-    listDescription: z.string(),
-    listCompletion: z.number(),
-  }),
-  async resolve({ input }) {
-    const listInDb = await prisma.list.create({
-      data: {
-        ...input,
-      },
-    });
-
-    return {
-      success: true,
-    };
-  },
-});
+export const appRouter = trpc
+  .router()
+  .query("get-list-by-id", {
+    async resolve() {
+      const lists = await prisma.list.findMany();
+      return {
+        lists,
+      };
+    },
+  })
+  .mutation("create-list-of-tasks", {
+    input: z.object({
+      listTitle: z.string(),
+      listDescription: z.string(),
+      listCompletion: z.number(),
+    }),
+    async resolve({ input }) {
+      console.log(input);
+      const createList = await prisma.list.create({
+        data: {
+          ...input,
+        },
+      });
+      return {
+        success: true,
+      };
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
